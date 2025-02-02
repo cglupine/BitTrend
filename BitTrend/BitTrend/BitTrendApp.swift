e86a67af1949f463409be896259290931c6d927a
exta0q2 <g.carbutto.guest@posteitaliane.it>
2025-02-02 10:37:23 +0100

    Create Xcode project

---
+++ BitTrend/BitTrend/BitTrendApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitTrendApp.swift
+//  BitTrend
+//
+//  Created by Gabriele Carbutto on 02/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BitTrendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
